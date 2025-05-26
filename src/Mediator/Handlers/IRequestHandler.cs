6f2e29faf00a7ebd547c46b4bf0133d32c018626
Lars Bruins Slot <lars.bruins.slot@joz.nl>
2025-05-26 19:25:40 +0200

    Changed interfaces and interface implementations in sample projects.

--- src/Mediator/Handlers/IRequestHandler.cs
+++ src/Mediator/Handlers/IRequestHandler.cs
@@ -6,5 +6,7 @@ public interface IRequestHandler<in TRequest, TResponse>
     ValueTask<TResponse> Handle(TRequest request, CancellationToken cancellationToken);
 }
 
-public interface IRequestHandler<in TRequest> : IRequestHandler<TRequest, Unit>
-    where TRequest : IRequest<Unit> { }
+public interface IRequestHandler<in TRequest>
+{
+    ValueTask Handle(TRequest request, CancellationToken cancellationToken);
+}
